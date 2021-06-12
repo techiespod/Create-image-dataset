cc16eb8ee6ee483caf1b25dd649018542568a71a
techiespod <76003539+techiespod@users.noreply.github.com>
2021-06-12 20:00:45 +0530

    Add files via upload

---
+++ multipleimage.py
@@ -0,0 +1,14 @@
+#Techiespod
+#Creating Image dataset
+
+from bing_image_downloader import downloader
+# Bing to extract images from internet
+
+query_string = ['Elon Musk', 'Obama', 'Jeff Bezos']
+# Here insert the names of object/person whose data is to be collected
+
+for content in query_string:
+    downloader.download(content, limit=10,  output_dir='Data set');
+# set limit for every object in list
+
+#for example if you need 10 images of each object present in list than set limit as per your need
