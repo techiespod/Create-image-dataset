5f23033177c1cf28bc5646f14d5cbccdcfcd0851
techiespod <76003539+techiespod@users.noreply.github.com>
2022-10-10 11:21:01 +0530

    Update multipleimage.py

--- multipleimage.py
+++ multipleimage.py
@@ -5,8 +5,7 @@ from bing_image_downloader import downloader
 # Bing to extract images from internet
 
 query_string = ['Elon Musk', 'Obama', 'Jeff Bezos']
-# Here insert the names of object/person whose data is to be collected
-
+# Here insert the names of object/perso
 for content in query_string:
     downloader.download(content, limit=10,  output_dir='Data set');
 # set limit for every object in list
